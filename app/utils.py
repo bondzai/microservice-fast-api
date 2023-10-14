6f31d80a1791d4315ab1350172f5f40627300102
JamesBond <introbond.dev@gmail.com>
2023-10-14 21:16:03 +0700

    Initial commit

---
+++ app/utils.py
@@ -0,0 +1,11 @@
+import time
+
+def performance_monitor(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Function '{func.__name__}' took {elapsed_time:.6f} seconds to execute.")
+        return result
+    return wrapper
