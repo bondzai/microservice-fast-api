66d34412630ad5b9d40606a864e372a1f38b4b85
JamesBond <introbond.dev@gmail.com>
2023-10-14 21:21:10 +0700

    Update: Utils class custom filters

--- app/utils.py
+++ app/utils.py
@@ -1,11 +1,28 @@
 import time
 
-def performance_monitor(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print(f"Function '{func.__name__}' took {elapsed_time:.6f} seconds to execute.")
+class Utils:
+
+    # input data: list of dict, search_keyword: string
+    def custom_filters(self, input_data, search_keyword):
+        result = []
+        search_columns = ["a", "b"]
+        
+        for project in input_data:
+            for key, value in project.items():
+                if key in search_columns:
+                    if search_keyword.lower() in str(value).lower():
+                        result.append(project)
+                        break
+
         return result
-    return wrapper
+
+    def performance_monitor(func):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(f"Function '{func.__name__}' took {elapsed_time:.6f} seconds to execute.")
+            return result
+
+        return wrapper
